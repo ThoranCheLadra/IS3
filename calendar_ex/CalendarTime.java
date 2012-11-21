598aa49e44723618ec9f8141a800fe4fa756deab
Cham <0902017a@student.gla.ac.uk>
2012-11-21 21:20:55 +0000

    added Arthur's work to repos

---
+++ calendar_ex/CalendarTime.java
@@ -0,0 +1,46 @@
+package calendar_ex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CalendarTime {
+	
+	// time object designed to work with the calendar class
+	
+	public int hr; // hour and minute for the time
+	public int min;
+	
+	public CalendarTime(int the_hr, int the_min){
+		hr = the_hr;
+		min = the_min;
+	}
+	
+        public static boolean isValidTimeString(String time){
+            Pattern p = Pattern.compile("[0-23]:[0-59]");
+            Matcher m = p.matcher(time);
+            if(m.matches()){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        
+	public static CalendarTime GetDuration(CalendarTime start_time, CalendarTime end_time){
+		// given the start and end of the meeting, get the duration.
+		
+		int start_mins = 60 * start_time.hr + start_time.min;
+		int end_mins = 60 * end_time.hr + end_time.min;
+		
+		int diff = end_mins - start_mins;
+		
+		return new CalendarTime(diff%60, diff / 60);
+		
+	}
+           @Override
+	public String toString() {
+		// turn the time into a string
+		
+		return "" + hr + ":" + min;
+	}
+}
